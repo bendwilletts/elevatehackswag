132a839151d70b21189ff11d932fb0e60941f1f5
bendwilletts <ben.d.willetts@gmail.com>
2018-09-22 15:34:38 -0400

    initial map page, with boostrap columns

--- static/main.js
+++ static/main.js
@@ -1,17 +1,16 @@
-
+var map;  
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
   var toronto = {lat: 43.6532, lng: -79.3832}; //43.6532° N, 79.3832
   // The map, centered at Uluru
-  var map = new google.maps.Map(
+  map = new google.maps.Map(
       document.getElementById('map'), {zoom: 10, center: toronto});
   // The marker, positioned at Uluru
   //var marker = new google.maps.Marker({position: toronto, map: map});
 }
-console.log("hello World");
+
 $(document).ready(function() {
 	
   initMap();
-  var key = "AIzaSyBKCC5OXs0cKovC04ht8K4LvXfwvvHDfEc";
 });
