599a136fa000c6be5607cc77ffa3005f7d5d9b69
upsilon <okrightbacktosleep@gmail.com>
2018-09-22 17:55:01 -0400

    button active styles

--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,13 @@ function initMap() {
   // The marker, positioned at Uluru
   //var marker = new google.maps.Marker({position: toronto, map: map});
 }
+
+function sidebar_click(button) {
+	button.classList.toggle("btn-light");
+	button.classList.toggle("btn-dark");
+	button.classList.toggle("active");
+}
+
 console.log("hello World");
 $(document).ready(function() {
 	
